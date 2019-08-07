450e9a9b1319a247ba37da3b15d786e6525b9243
sam <sammebordeleau@hotmail.com>
2019-08-07 17:35:14 -0400

    minor edits

--- js/ux.js
+++ js/ux.js
@@ -1,3 +1,4 @@
+// CLOCK
 function startTime() {
   var today = new Date();
   var h = today.getHours();
@@ -10,15 +11,54 @@ function startTime() {
   var t = setTimeout(startTime, 500);
 }
 function checkTime(i) {
-  if (i < 10) {i = "0" + i}; 
+  if (i < 10) {i = "0" + i};
   return i;
 }
+// ABOUT: POP-UP MODALS
 $(function() {
   $( "#modal1").draggable();
   $( "#modal2").draggable();
   $( "#modal3").draggable();
   $( "#modal4").draggable();
 } );
+// SCHEDULE: BUTTON SWITCH
+$(function(){
+  $("#monday-button").click(function(){
+  $("#monday-list").show();
+  $("#tuesday-list").hide();
+  $("#wednesday-list").hide();
+  $("#thursday-list").hide();
+  $("#friday-list").hide();
+});
+  $("#tuesday-button").click(function(){
+  $("#tuesday-list").show();
+  $("#monday-list").hide();
+  $("#wednesday-list").hide();
+  $("#thursday-list").hide();
+  $("#friday-list").hide();
+});
+  $("#wednesday-button").click(function(){
+  $("#wednesday-list").show();
+  $("#tuesday-list").hide();
+  $("#monday-list").hide();
+  $("#thursday-list").hide();
+  $("#friday-list").hide();
+});
+  $("#thursday-button").click(function(){
+  $("#thursday-list").show();
+  $("#wednesday-list").hide();
+  $("#tuesday-list").hide();
+  $("#monday-list").hide();
+  $("#friday-list").hide();
+});
+  $("#friday-button").click(function(){
+  $("#friday-list").show();
+  $("#thursday-list").hide();
+  $("#wednesday-list").hide();
+  $("#tuesday-list").hide();
+  $("#monday-list").hide();
+});
+// ARCHIVE: BUTTON SWITCH
 $(function(){
   $("#show-button").click(function(){
   $("#show-list").show();

91a23ee5ebc62bade2b04ee4b327e5c8df5cdc15
Matthew Halpenny <matt.halpenny@gmail.com>
2019-09-15 16:13:38 -0400

    node.js server merge
    updated modules / package-lock
    app.js primary server
    added lib directory

--- app.js
+++ app.js
@@ -1,19 +1,63 @@
+//REQUIRE
 var fs = require('fs');
 var express = require('express');
-// let ejs = require('ejs');
-// var app = express();
+var bodyParser = require('body-parser');
 
-// app.set('view engine', 'ejs');
-// app.use('/assets', express.static('forms'));
+//launch express function
+var app = express();
 
-// app.get('/', function(req, res){
-// res.render('submission');
-// });
+//tell node.js which view engine we will be using when calling views
+app.set('view engine', "ejs");
+//create application/x-www-form-urlencoded parser
+var urlencodedParser = bodyParser.urlencoded({ extended: false });
 
-// app.get();
-
-
-// this line of code creates an another file output.txt and writes the data in sample into the log.
-fs.writeFileSync('output.txt', "hello");
-
+//opening a listening port
 app.listen(3000);
+console.log('listening on port 3000');
+
+//use ~these~ directories when serving an html file
+//automatically sends contents to the viewer as the html will not load them when served by node
+app.use("/css",  express.static(__dirname + '/css'));
+app.use("/js", express.static(__dirname + '/js'));
+app.use("/assets",  express.static(__dirname + '/assets'));
+app.use("/fonts",  express.static(__dirname + '/fonts'));
+app.use("/lib",  express.static(__dirname + '/lib'));
+
+//GETS
+//upon navigating the site send corresponding html pages to client
+app.get("/", function(req, res){
+res.sendFile(__dirname + '/index.html');
+});
+
+app.get("/index.html", function(req, res){
+  res.sendFile(__dirname + '/index.html');
+  });
+
+app.get("/about.html", function(req, res){
+  res.sendFile(__dirname + '/about.html');
+  });
+
+app.get("/schedule.html", function(req, res){
+  res.sendFile(__dirname + '/schedule.html');
+  //when requesting this page automatically include this excel file
+  app.get('/test.xlsx',function(req,res){
+    res.sendFile(__dirname + '/test.xlsx'); 
+});
+  });
+
+app.get("/player.html", function(req, res){
+  res.sendFile(__dirname + '/player.html');
+  });
+
+//send a view template for user submission data
+app.get("/submission.html", function(req, res){
+  res.render('submission', {qs: req.query});
+  });
+
+app.post("/submission.html", urlencodedParser, function(req, res){
+  //console.log(req.body);
+  res.render('submission', {qs: req.query}); 
+  fs.mkdirSync('./submissions/' + req.body.lastname + req.body.firstname);
+  fs.writeFileSync('./submissions/' + req.body.lastname + req.body.firstname + "/info.JSON", JSON.stringify(req.body, null, "\t"));
+});
+

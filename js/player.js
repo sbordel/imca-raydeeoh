d4b801bc4faa4092f87e61519da30de15c8cd17f
Matthew Halpenny <matt.halpenny@gmail.com>
2019-12-02 10:48:35 -0500

    Player.js

--- js/player.js
+++ js/player.js
@@ -1,29 +1,29 @@
 	//open stream from VPS
-	// var myaudio = new Audio('http://rhizotron.net:8080/listen.mp3');
-	// //global variables
-	// let init = false;
-	// let status = true;
+	var myaudio = new Audio('http://rhizotron.net:8080/listen.mp3');
+	//global variables
+	let init = false;
+	let status = true;
 
 
 	function play(){
-		// if (init === false){
-		// myaudio.play(); 
-		// init = true;
-		// console.log("stream opened!");	
-		// }
+		if (init === false){
+		myaudio.play(); 
+		init = true;
+		console.log("stream opened!");	
+		}
 
-		// if (status === true){
-		// myaudio.muted = false;
-		// status = false;
-		// // console.log(status);
-		// document.getElementById("volumeon-icon").src = "assets/audio_player_files/volumeonbutton.png";
+		if (status === true){
+		myaudio.muted = false;
+		status = false;
+		// console.log(status);
+		document.getElementById("volumeon-icon").src = "assets/audio_player_files/volumeonbutton.png";
 
-		// } else if (status === false){
-		// 	myaudio.muted = true;
-		// 	status = true;
-		// 	// console.log(status);
-		// 	document.getElementById("volumeon-icon").src = "assets/audio_player_files/volumeoffbutton.png";
-		// }
+		} else if (status === false){
+			myaudio.muted = true;
+			status = true;
+			// console.log(status);
+			document.getElementById("volumeon-icon").src = "assets/audio_player_files/volumeoffbutton.png";
+		}
 
 	// 	$('#volumeon-icon').on('click', function(){
 
@@ -76,6 +76,8 @@ var scheduleTime;
 	} 
 	
 	textBar(scheduleTime);
+	console.log(scheduleTime);
+	console.log("sending time to player");
 }
 
 function checkTime(i) {

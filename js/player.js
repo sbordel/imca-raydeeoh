63142a4c529fe94f8e7210ecae09b8458771f38a
Matthew Halpenny <matt.halpenny@gmail.com>
2020-01-18 14:39:39 -0500

    mobile schedule

--- js/player.js
+++ js/player.js
@@ -1,5 +1,5 @@
 	let dayInit = false;
-	
+
 	//function for index/player play button
 	function play() {
 		if (init === false) {
@@ -34,74 +34,75 @@
 		//index to determine whats playing in regards to the schedule
 		var scheduleTime;
 
-		//queries time (located in ux.js)
+		//queries time every half second
 		m = checkTime(m);
 		s = checkTime(s);
 
 		document.getElementById('clock').innerHTML =
-		h + ":" + m + ":" + s;
-	  	var t = setTimeout(startTime, 500);
+			h + ":" + m + ":" + s;
+		var t = setTimeout(startTime, 500);
 
 		//check dotw, fill schedule (only runs once per load)
-		if (dayInit == false){
-		if (day == "Monday") {
-			monday();
-			$("#monday-button").css('background-color', 'black');
-			$("#monday-button").css('color', 'white');
-		} else if (day == "Tuesday") {
-			tuesday();
-			$("#tuesday-button").css('background-color', 'black');
-			$("#tuesday-button").css('color', 'white');
-		} else if (day == "Wednesday") {
-			wednesday();
-			$("#wednesday-button").css('background-color', 'black');
-			$("#wednesday-button").css('color', 'white');
-		} else if (day == "Thursday") {
-			thursday();
-			$("#thursday-button").css('background-color', 'black');
-			$("#thursday-button").css('color', 'white');
-		} else if (day == "Friday") {
-			friday();
-			$("#friday-button").css('background-color', 'black');
-			$("#friday-button").css('color', 'white');
-		} else if (day == "Saturday" || "Sunday") {
+		if (dayInit == false) {
+			if (day == "Monday") {
+				monday();
+				$("#monday-button").css('background-color', 'black');
+				$("#monday-button").css('color', 'white');
+			} else if (day == "Tuesday") {
+				tuesday();
+				$("#tuesday-button").css('background-color', 'black');
+				$("#tuesday-button").css('color', 'white');
+			} else if (day == "Wednesday") {
+				wednesday();
+				$("#wednesday-button").css('background-color', 'black');
+				$("#wednesday-button").css('color', 'white');
+			} else if (day == "Thursday") {
+				thursday();
+				$("#thursday-button").css('background-color', 'black');
+				$("#thursday-button").css('color', 'white');
+			} else if (day == "Friday") {
+				friday();
+				$("#friday-button").css('background-color', 'black');
+				$("#friday-button").css('color', 'white');
+			} else if (day == "Saturday" || "Sunday") {
+				scheduleTime = 99;
+			}
+			dayInit = true;
+		}
+		if (day == "Saturday" || "Sunday") {
 			scheduleTime = 99;
-		} 
-		dayInit = true;
-	}
-
-		//check dotw, calculate what row of schedule to check for data
-		if (day == "Monday") {
-			tempMath = (h - 10);
-			scheduleTime = tempMath;
-		} else if (day == "Tuesday") {
-			tempMath = 8 + (h - 10);
-			scheduleTime = tempMath;
-		} else if (day == "Wednesday") {
-			tempMath = 16 + (h - 10);
-			scheduleTime = tempMath;
-		} else if (day == "Thursday") {
-			tempMath = 24 + (h - 10);
-			scheduleTime = tempMath;
-		} else if (day == "Friday") {
-			tempMath = 32 + (h - 10);
-			scheduleTime = tempMath;
-		} 
-
-		//if its after 6 or before 10, go off air
-		if (h >= 18 || h < 10) {
-		scheduleTime = 99;
 		}
 
-		//update text in both player sections (schedule.js)
-		textBar(scheduleTime);
-	}
+			//check dotw, calculate what row of schedule to check for data
+			if (day == "Monday") {
+				tempMath = (h - 10);
+				scheduleTime = tempMath;
+			} else if (day == "Tuesday") {
+				tempMath = 8 + (h - 10);
+				scheduleTime = tempMath;
+			} else if (day == "Wednesday") {
+				tempMath = 16 + (h - 10);
+				scheduleTime = tempMath;
+			} else if (day == "Thursday") {
+				tempMath = 24 + (h - 10);
+				scheduleTime = tempMath;
+			} else if (day == "Friday") {
+				tempMath = 32 + (h - 10);
+				scheduleTime = tempMath;
+			}
 
-	function checkTime(i) {
-		if (i < 10) {
-		  i = "0" + i
-		};
-		return i;
-	  }
+			//if its after 6 or before 10, go off air
+			if (h >= 18 || h < 10) {
+				scheduleTime = 99;
+			}
 
-	
+			//update text in both player sections (schedule.js)
+			textBar(scheduleTime);
+		}
+
+		function checkTime(i) {
+			if (i < 10) {
+				i = "0" + i
+			};
+			return i;
+		}
--- js/schedule.js
+++ js/schedule.js
@@ -16,76 +16,105 @@ var dayData = [0, 0];
 
 
 //Onload, grab data from the schedule excel file
-oReq.onload = function(e) {
+oReq.onload = function (e) {
   var arraybuffer = oReq.response;
 
   /* convert data to binary string */
   var data = new Uint8Array(arraybuffer);
   var arr = new Array();
-  for(var i = 0; i != data.length; ++i) arr[i] = String.fromCharCode(data[i]);
+  for (var i = 0; i != data.length; ++i) arr[i] = String.fromCharCode(data[i]);
   var bstr = arr.join("");
 
   /* Call XLSX */
-  var workbook = XLSX.read(bstr, {type:"binary"});
+  var workbook = XLSX.read(bstr, {
+    type: "binary"
+  });
   var first_sheet_name = workbook.SheetNames[0];
 
   /* Get worksheet */
   worksheet = workbook.Sheets[first_sheet_name];
 
   /* Display and save worksheet as usable JSON data */
-  console.log(XLSX.utils.sheet_to_json(worksheet,{raw:true}));
-  radioObject = XLSX.utils.sheet_to_json(worksheet,{raw:true});
+  console.log(XLSX.utils.sheet_to_json(worksheet, {
+    raw: true
+  }));
+  radioObject = XLSX.utils.sheet_to_json(worksheet, {
+    raw: true
+  });
 }
 
 oReq.send();
 
- /* the fill function replaces info within the schedule's HTML table */
-function fill(){
-  var j=1;
-$(document).ready(function(){
-  //populates schedule row data for i, j will keep track of the interations and HTML rows
-  for(i=dayData[0];i<dayData[1];i++){
-  $(".show-name", "#row"+(j)).text(radioObject[i].Show);
-  $(".host-name", "#row"+(j)).text(radioObject[i].Artist);
-  $(".show-description", "#row"+(j)).text(radioObject[i].Description);
-  ++j;
-}});}
+/* the fill function replaces info within the schedule's HTML table */
+function fill() {
+  var j = 1;
+  $(document).ready(function () {
+    // if ((/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) || window.innerWidth < 500) {
+      if (window.innerWidth < 800) {  
+    //populates schedule row data in MOBILE for i, j will keep track of the interations and HTML rows
+      for (i = dayData[0]; i < dayData[1]; i++) {
+        $("#show-name", "#row" + (j)).text(radioObject[i].Show);
+        $("#host-name", "#row" + (j)).text(radioObject[i].Artist);
+        ++j;
+      }
+    } else {
+      //populates schedule row data for i, j will keep track of the interations and HTML rows
+      for (i = dayData[0]; i < dayData[1]; i++) {
+        $(".show-name", "#row" + (j)).text(radioObject[i].Show);
+        $(".host-name", "#row" + (j)).text(radioObject[i].Artist);
+        $(".show-description", "#row" + (j)).text(radioObject[i].Description);
+        ++j;
+      }
+    }
+  });
+}
 
 // ********** mobile max-width: 800px **********
 
 //Updates textbar information
 //timeData is a single value corresponding to a row in the excel schedule
-function textBar(timeData){
+function textBar(timeData) {
   // console.log("textbar populated")
-  if (timeData === 99){
+  if (timeData === 99) {
     $("#external-player-text").text("OFF AIR");
     $("#player-text").text("OFF AIR");
     $("#player-modal").text("OFF AIR");
-  } else{
+  } else {
     $("#external-player-text").text(radioObject[timeData].Artist + " - " + radioObject[timeData].Show);
     $("#player-text").text(radioObject[timeData].Artist + " - " + radioObject[timeData].Show);
     $("#player-modal").text(radioObject[timeData].Artist + " - " + radioObject[timeData].Show);
-  }}
+  }
+}
 
 
 //Day functions for schedule buttons
-function monday(){
+function monday() {
   dayData = monData;
   fill();
 }
-function tuesday(){
+
+function tuesday() {
   dayData = tuesData;
   fill();
 }
-function wednesday(){
+
+function wednesday() {
   dayData = wedData;
   fill();
 }
-function thursday(){
+
+function thursday() {
   dayData = thursData;
   fill();
 }
-function friday(){
+
+function friday() {
   dayData = friData;
   fill();
+}
+
+//redefine W H if the window is resized
+function reportWindowSize() {
+  heightOutput.textContent = window.innerHeight;
+  widthOutput.textContent = window.innerWidth;
 }
